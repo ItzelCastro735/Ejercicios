1663fea343bf486a5d21732e2f1f9112463f337a
ItzelCastro735 <itzelmarisolcastro@gmail.com>
2024-09-26 16:04:18 -0600

    primer commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Cast!");
+
+// Cast Implicit
+
+int valor = 2147483647;
+
+long number = valor;
+
+console.WriteLine(number);
+
+//cast explicit
